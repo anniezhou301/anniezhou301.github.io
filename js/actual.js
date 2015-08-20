949dca3df6e89741f1de5d7ded258266a975f681
Annie <anniezhou301@gmail.com>
2015-08-19 22:11:01 -0400

    Trying to get animation to work

--- js/actual.js
+++ js/actual.js
@@ -1,12 +1,18 @@
 
-		$(document).ready(function(){
-			$('label'.click(function(){
-
-						var total = 0;
-
-			$('.options:checked').each(function(){
-				total+=parseInt($(this).val());
-			});
-			$('#total').html('$'+total);
-		});
-		});
+// retrieve the element
+element = document.getElementById("fade");
+nav = document.getElementById("nav");
+// reset the transition by...
+nav.addEventListener("click", function(e){
+  e.preventDefault;
+  
+  // -> removing the class
+  element.classList.remove("animated");
+  
+  // -> triggering reflow /* The actual magic */
+  // without this it wouldn't work. Try uncommenting the line and the transition won't be retriggered.
+  element.offsetWidth = element.offsetWidth;
+  
+  // -> and re-adding the class
+  element.classList.add("animated");
+}, false);
